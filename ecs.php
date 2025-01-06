6c54891683a06e7a2882130c72b98ea1cc6c1b61
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2025-01-06 20:17:48 +0100

    remove idea files

--- ecs.php
+++ ecs.php
@@ -3,12 +3,17 @@
 declare(strict_types=1);
 
 /*
- * CoreShop GmbH
+ * CoreShop
  *
- * This software is available under the CoreShop Commercial License (CCL).
+ * This source file is available under two different licenses:
+ *  - GNU General Public License version 3 (GPLv3)
+ *  - CoreShop Commercial License (CCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
  *
  * @copyright  Copyright (c) CoreShop GmbH (https://www.coreshop.com)
- * @license    https://www.coreshop.com/license CCL
+ * @license    https://www.coreshop.com/license     GPLv3 and CCL
+ *
  */
 
 use PhpCsFixer\Fixer\Alias\EregToPregFixer;
