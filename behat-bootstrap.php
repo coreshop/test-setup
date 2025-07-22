1fb00b597002dd489099142f308ba957301d690d
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2025-07-22 13:57:28 +0200

    update for symfony 7

--- behat-bootstrap.php
+++ behat-bootstrap.php
@@ -2,15 +2,9 @@
 /*
  * CoreShop
  *
- * This source file is available under two different licenses:
- *  - GNU General Public License version 3 (GPLv3)
- *  - CoreShop Commercial License (CCL)
+ * This source file is available under the MIT license
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
- *
- * @copyright  Copyright (c) CoreShop GmbH (https://www.coreshop.com)
- * @license    https://www.coreshop.com/license     GPLv3 and CCL
- *
  */
 
 if (!defined('PIMCORE_PROJECT_ROOT')) {
